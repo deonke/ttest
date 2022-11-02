9627ddce3166bf1678cffcedc648a5c4f111d63d
deonke <117237980+deonke@users.noreply.github.com>
2022-11-02 09:28:32 +0900

    Update name.c

--- name.c
+++ name.c
@@ -1,4 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-int main(){
-  printf("Hello!");
-  }
+void write_file() {
+	FILE* fp = fopen("test.txt", "w");
+	char str[] = "Hello World!";
+
+	fputs(str, fp);
+
+	fclose(fp);
+}
+
+void read_file() {
+	FILE* fp = fopen("test.txt", "r");
+	char str[100];
+
+	fgets(str, sizeof(str), fp);
+	printf("%s", str);
+	
+	fclose(fp);
+}
+int main() {
+	write_file();
+	read_file();
+}
